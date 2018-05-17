84d2e60ca578e914a8f61ded8f24d4e9eb18c5a6
Stephen Pham <spham@thoughtworks.com>
2018-05-17 23:37:23 +1000

    Install preq for vim

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -5,7 +5,12 @@ set -eo pipefail
 # Copy vim config
 cp .vimrc ~/.
 
+# Install Pathogen
+mkdir -p ~/.vim/autoload
+wget -P ~/.vim/autoload https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
 # Install solarized theme
+mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
 git clone git://github.com/altercation/vim-colors-solarized.git
 mv vim-colors-solarized ~/.vim/bundle/
