6483640653ebf7aa747ffe2224dc4dbff68365dd
Stephen Pham <pham.stephen@gmail.com>
2018-05-23 23:41:20 +1000

    Use menlo powerline font for iterm2

--- iterm2/setup_iterm2.sh
+++ iterm2/setup_iterm2.sh
@@ -9,3 +9,6 @@ brew cask install iterm2
 git clone https://github.com/powerline/fonts.git ~/fonts
 sh ~/fonts/install.sh
 rm -rf ~/fonts
+
+# Install menlo powerline font
+git clone https://github.com/abertsch/Menlo-for-Powerline.git
