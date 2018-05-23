dbc57531cd19276575f921e15ab2c5dc5c2b4165
Stephen Pham <pham.stephen@gmail.com>
2018-05-24 07:39:40 +1000

    Add default installs for vscode extensions

--- vscode/setup_vscode.sh
+++ vscode/setup_vscode.sh
@@ -5,5 +5,10 @@ set -eo pipefail
 # Install vscode
 brew cask install visual-studio-code
 
+# Install extensions
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension monokai.theme-monokai-pro-vscode
+code --install-extension robertohuertasm.vscode-icons
+
 # Copy user settings.json
 cp settings.json ~/Library/Application\ Support/Code/User/settings.json
