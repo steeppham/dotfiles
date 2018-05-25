5458b388b801a7eefab8b1ff6537bd5dfd213459
Stephen Pham <pham.stephen@gmail.com>
2018-05-25 10:40:48 +1000

    Fix vscode nvm issue in the integrated terminal

--- vscode/setup_vscode.sh
+++ vscode/setup_vscode.sh
@@ -8,7 +8,6 @@ brew cask install visual-studio-code
 # Install extensions
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension monokai.theme-monokai-pro-vscode
-code --install-extension robertohuertasm.vscode-icons
 
 # Copy user settings.json
 cp settings.json ~/Library/Application\ Support/Code/User/settings.json
