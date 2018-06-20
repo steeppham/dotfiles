80ca0d05a2ec14eb388ce341c12e33565fbf7d44
Stephen Pham <pham.stephen@gmail.com>
2018-06-21 05:37:34 +1000

    Check xcode command line tools is installed

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+set -eo pipefail
+
 echo "Setting up..."
 
 # Install xcode command line tools
-xcode-select --install
+if test ! $(xcode-select -p) ; then
+  echo "Installing xcode command line tools..."
+  xcode-select --install
+fi
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
+  echo "Installing homebrew.."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
@@ -14,5 +20,6 @@ fi
 brew update
 
 # Install brew packages (Brewfile)
+echo "Installing brew packages..."
 brew bundle
 brew doctor
