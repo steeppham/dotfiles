f2f738699a2c1671d21b7138c343f33de0fa1f65
Stephen Pham <pham.stephen@gmail.com>
2018-07-03 22:20:53 +1000

    Remove extension customisation for vscode

--- vscode/setup_vscode.sh
+++ vscode/setup_vscode.sh
@@ -6,8 +6,8 @@ set -eo pipefail
 brew cask install visual-studio-code
 
 # Install extensions
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension monokai.theme-monokai-pro-vscode
+#code --install-extension dbaeumer.vscode-eslint
+#code --install-extension monokai.theme-monokai-pro-vscode
 
 # Copy user settings.json
 cp settings.json ~/Library/Application\ Support/Code/User/settings.json
