fbb5a07851818d3e34e46c692e7128b6e7f0bc0f
Stephen Pham <pham.stephen@gmail.com>
2018-07-03 21:29:49 +1000

    Install node using nvm installer

---
+++ setup_node.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eo pipefail
+
+echo "Installing nvm..."
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+echo "Installing node..."
+nvm install --lts
