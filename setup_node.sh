0acf45d7a0be727dd4c59a25e50585df98071b1a
Stephen Pham <pham.stephen@gmail.com>
2019-11-12 11:45:18 +1100

    Update setup for nvm

--- setup_node.sh
+++ setup_node.sh
@@ -4,10 +4,6 @@ set -eo pipefail
 
 echo "Installing nvm..."
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
 
-echo "Installing node..."
-nvm install --lts
+#nvm install --lts
